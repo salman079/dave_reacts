c2efc7076ad4f0987430609a97555998646c6e99
salmansiddiqui <salman079@gmail.com>
2020-09-25 01:47:23 +0300

    material UI

--- dave_react_3/src/index.js
+++ dave_react_3/src/index.js
@@ -1,8 +1,23 @@
 import React, { useState } from "react";
 import ReactDOM from "react-dom";
 import "./styles.css";
+import PlusOneIcon from '@material-ui/icons/PlusOne';
+import ExposureNeg1Icon from '@material-ui/icons/ExposureNeg1';
+import FormatIndentIncreaseIcon from '@material-ui/icons/FormatIndentIncrease';
+import FormatIndentDecreaseIcon from '@material-ui/icons/FormatIndentDecrease';
+import Button from '@material-ui/core/Button';
+import { makeStyles } from '@material-ui/core/styles';
+
+const useStyles = makeStyles((theme) => ({
+  root: {
+    '& > *': {
+      margin: theme.spacing(1),
+    },
+  },
+}));
 
 function Room() {
+  const classes = useStyles();
   const [typedName, setTypedName] = React.useState("");
   const [enteredName, setEnteredName] = React.useState("");
   const inputName = (e) => {
@@ -23,8 +38,10 @@ function Room() {
       <button onClick={() => setLit(true)} onDoubleClick={DoubleClick}> light On </button>
       <button onClick={() => setLit(false)}> Light Off </button> <br />
       The current temprature is {temp}
-      <button onClick={() => setTemp(temp + 1)}> + </button>
-      <button onClick={() => setTemp(temp - 1)}> - </button> <br />
+      <div className={classes.root}>
+      <Button onClick={() => setTemp(temp + 1)}> <PlusOneIcon /> </Button>
+      <button onClick={() => setTemp(temp - 1)}> <ExposureNeg1Icon /> </button> <br />
+      </div>
       Counter value is {count1}
       <form>
         change by <input type="number"
@@ -36,8 +53,8 @@ function Room() {
         >
         </input>
       </form>
-      <button onClick={() => setCount(count1 + 1)}> Increase </button>
-      <button onClick={() => setCount(count1 - 1)}> Decrease </button>
+      <button onClick={() => setCount(count1 + 1)}> <FormatIndentIncreaseIcon /> </button>
+      <button onClick={() => setCount(count1 - 1)}> <FormatIndentDecreaseIcon /> </button>
       <br />
 Typed Name is {typedName} <br />
 Entered Name is {enteredName} <br />
@@ -46,25 +63,30 @@ Entered Name is {enteredName} <br />
         value={typedName}
       />
       <br />
-      <button onClick={enterName}> Enter  </button><br />
+      <div className={classes.root}>
+      <Button variant="contained" color="secondary" onClick={enterName}> Enter  </Button><br />
+    </div>
     </div>
   );
 }
 
 function OneTimeButton(props) {
   const [clicked, setClicked] = useState(false);
-
+  const classes = useStyles();
   function doClick() {
     // props.onClick();
     setClicked(true);
   }
   return (
-    <button
+    <div className={classes.root}>
+    
+    <Button variant="contained" color="primary"
       onClick={clicked ? undefined : doClick}
       disabled={clicked}
     >
       You Can Only Click Me Once
-    </button>
+    </Button>
+    </div>
   );
 }
 ReactDOM.render(

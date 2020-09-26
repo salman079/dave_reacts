406bb6b9c5733201331758873fbe6916be9f69d7
salmansiddiqui <salman079@gmail.com>
2020-09-26 16:09:55 +0300

    exp tracker app update

--- cashflow/src/index.js
+++ cashflow/src/index.js
@@ -1,12 +1,14 @@
-import React from 'react';
+import React, { useState } from 'react';
 import ReactDOM from 'react-dom';
+import './index.css';
 
 function App() {
-  const [CashFlow, setCashflow] = React.useState();
-  const [inflow, setInflow] = React.useState(0);
-  const [outflow, setOutflow] = React.useState(0);
-  const [nature, setNature] = React.useState("");
-  const [trsflow, setTrsflow] = React.useState([]);
+  const [CashFlow, setCashflow] = useState();
+  const [inflow, setInflow] = useState(0);
+  const [outflow, setOutflow] = useState(0);
+  const [nature, setNature] = useState("");
+  // const [trsflow, setTrsflow] = useState([]);
+  const [trsflow, setTrsflow] = useState([{ desc:'' , amount:0}]);
 
   const inputTxt = (e) => {
     setNature(e.target.value);
@@ -20,14 +22,13 @@ function App() {
     setCashflow(0);
   }
   const outdflow = (e) => {
-    setOutflow(outflow + parseInt(CashFlow, 10))
+    setOutflow(outflow + parseInt(CashFlow, 10));
     // setBalance(inflow+outflow);
     setCashflow(0);
   }
   const cashdflow = (e) => {
     e.preventDefault();
-    setTrsflow(trsflow.concat(nature + " of SR " + CashFlow));
-    console.log(trsflow);
+    setTrsflow(trsflow => [...trsflow, {desc: nature, amount: CashFlow}]);
     if (CashFlow > 0) {
       indflow();
     }
@@ -36,22 +37,58 @@ function App() {
     }
   }
   return (<>
-    <form onSubmit={cashdflow}>
-      <h1>Cash Flow</h1>
-    Balance :SR {inflow + outflow} <br />
-    Total Inflow :SR {inflow} <br />
-    Total Outflow :SR {outflow} <br />
-      <h1>Transaction History</h1><br /> <ul>{trsflow.map(city => <li key={city}>{city}</li>)}</ul> <br />
+    <div className="main-container">
+
+      <h1 className='header'>Cash Flow</h1>
+      <h3>Balance :SR {inflow + outflow}</h3> <br />
+      <div className='in-ex-container'>
+        <h4>Total Inflow <br /><div className="inflow">
+          SR {inflow}
+        </div>
+        </h4>
+        <h4>Total Outflow <br />
+          <div className="outflow">
+            SR {outflow}
+          </div>
+        </h4>
+        <br />
+      </div>
+      <h2>Transaction History</h2><hr /><br />
+      <ul className="transaction-list">
+        {trsflow.map((transaction, ind) => {
+          return (
+            <li key={ind}>
+              {/* <span>{transaction}</span> */}
+              <span>{transaction.desc}</span>
+              <span>{transaction.amount}</span>
+            </li>)
+        })}
+      </ul>
+      <br />
       <h2>Add New Transaction</h2>
-    Amount <input type="number" placeholder="cash flow"
-        onChange={inputValue}
-        value={CashFlow} /> <br />
-    Nature of transaction <input type="text" placeholder="Natrue of transaction"
-        onChange={inputTxt}
-        value={nature} /> <br />
-      <button type="submit" >
-        Cashflows  </button><br />
-    </form>
+      <form onSubmit={cashdflow} className="transaction-form">
+        <label>Amount </label>
+        <input
+          type="number"
+          placeholder="cash flow"
+          onChange={inputValue}
+          value={CashFlow}
+          required
+        /> <br />
+        <label>Nature of transaction </label>
+        <input
+          type="text"
+          placeholder="Natrue of transaction"
+          onChange={inputTxt}
+          value={nature}
+          required
+        /> <br />
+        <button
+          className="btn"
+          type="submit" >
+          Add Transaction  </button><br />
+      </form>
+    </div>
   </>
   )
 };
